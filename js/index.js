aab0a4efa5bd4c0d4d891484438ae712b69453f8
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-08-17 13:34:11 -0300

    aproveitando menu

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 import initMenu from "./openMenu.js"
 import initModalLogin from "./openModalLogin.js"
+import initMenuRiot from "./openMenuRiot.js"
 
 initMenu();
 initModalLogin();
+initMenuRiot();
